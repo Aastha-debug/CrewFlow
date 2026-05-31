2869e50b1c961358a02902f203a73bb456315eca
Aastha <aastha@example.com>
2026-05-31 19:24:52 +0530

    feat: initial commit for CrewFlow team task manager

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -391,7 +391,6 @@ function AppContent() {
       
       {/* Sticky Top Grayscale Navbar (Spans full-width, stationary) */}
       <Header 
-        activeModule={activeModule}
         activeView={activeView}
         activeProjectId={activeProjectId}
         projects={projects}
